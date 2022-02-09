f784a13102626e90a7f44be59b0540d16072b91d
wezhall <wesleyericksonhall@gmail.com>
2022-02-09 23:21:00 +0000

    Updated follos js file

--- js4exe/follow.js
+++ js4exe/follow.js
@@ -52,9 +52,11 @@
 const birthYear = 1948;
 const thisYear = 1965;
 const firstName = "Carlos";
-const lastName = "Stevenson";
+const lastName = " Stevenson";
+const fullName = firstName + lastName;
+const age = thisYear - birthYear;
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 console.log(greeting);
 
