a054d27c8f150557e4476a0ddab591eda444fd67
wezhall <wesleyericksonhall@gmail.com>
2022-02-16 21:43:35 +0000

    Added pseudocode

--- R-P-S/script.js
+++ R-P-S/script.js
@@ -1,7 +1,7 @@
 function computerPlay() {
-	Return randomly Rock, Paper or Scissors i.e return random word from list
+	//Return randomly Rock, Paper or Scissors i.e return random word from list
 }
 
 function singleRound (playerSelection, computerSelection) {
-	Return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+	//Return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 }
