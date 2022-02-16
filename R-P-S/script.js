92f641813f52cafd00b5902450cb710eed0567ea
wezhall <wesleyericksonhall@gmail.com>
2022-02-16 21:32:14 +0000

    Added computerPlay and singleRound functions to script

---
+++ R-P-S/script.js
@@ -0,0 +1,7 @@
+function computerPlay() {
+	Return randomly Rock, Paper or Scissors
+}
+
+function singleRound (playerSelection, computerSelection) {
+	Return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+}
