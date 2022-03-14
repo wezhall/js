e5c5ba0a86003fbfc19f9ad12de693e48f14d7aa
wezhall <wesleyericksonhall@gmail.com>
2022-03-14 19:14:56 +0000

    Returned random element of array

--- R-P-S/script.js
+++ R-P-S/script.js
@@ -1,6 +1,8 @@
 function computerPlay() {
 	//Return randomly Rock, Paper or Scissors i.e return random word from list
 	const myArray = ['Rock', 'Paper', 'Scissors'];
+
+	return myArray[Math.floor(Math.random() * 3)];
 }
 
 function singleRound (playerSelection, computerSelection) {
