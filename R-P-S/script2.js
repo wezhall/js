04148c3e1e0a48ac66688f755087bde05c0c31f7
wezhall <wesleyericksonhall@gmail.com>
2022-03-20 22:12:53 +0000

    Updated rock paper scissorcs game script

---
+++ R-P-S/script2.js
@@ -0,0 +1,68 @@
+function computerPlay() {
+	//Return randomly Rock, Paper or Scissors i.e return random word from list
+	const myArray = ['Rock', 'Paper', 'Scissors'];
+
+	return myArray[Math.floor(Math.random() * 3)];
+}
+
+function singleRound (playerChoice, computerSelection) {
+	//Return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+	var playerSelection = playerChoice.toUpperCase();
+
+	if (playerSelection == 'ROCK') {
+		if (computerPlay() == 'Rock') {
+			return "It's a tie!"
+		}
+
+		else if (computerPlay() == 'Scissors') {
+			return "You win! Rock beats Scissors!"
+		}
+
+		else if (computerPlay() == 'Paper') {
+			return "You lose. Paper beats Rock."
+		}
+
+		else {
+			return "random"
+		}
+
+	}
+
+	else if (playerSelection == 'SCISSORS') {
+		if (computerPlay() == 'Rock') {
+			return "You lose. Rock beats Scissors"
+		}
+
+		else if (computerPlay() == 'Scissors') {
+			return "It's a tie!"
+		}
+
+		else if (computerPlay() == 'Paper') {
+			return "You win! Scissors beats Paper"
+		}
+
+	}
+
+	else if (playerSelection == 'PAPER') {
+		if (computerPlay() == 'Rock') {
+			return "You win! Paper beats Rock"
+		}
+
+		else if (computerPlay() == 'Scissors') {
+			return "You lose. Scissors beats Paper"
+		}
+
+		else if (computerPlay() == 'Paper') {
+			return "It's a tie!"
+		}
+
+	}
+
+}
+
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+
+console.log(singleRound(playerSelection,computerSelection));
+console.log(computerPlay())
